0020d1bc2896578ec35f8a6f172fb9f825651f4f
阮鸿波 <aronruan@kugou.net>
2020-07-22 17:28:18 +0800

    feat: add and-deisgn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import '../assets/variables.less'
 import { AppProps } from 'next/app'
-
+import { BasicLayout, PageContainer, PageLoading } from '@ant-design/pro-layout'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  if (pageProps.statusCode === 404) return <Component {...pageProps} />
+  return (
+    <BasicLayout>
+      <PageContainer content={<Component {...pageProps} />}></PageContainer>
+    </BasicLayout>
+  )
 }
 
 export default MyApp
